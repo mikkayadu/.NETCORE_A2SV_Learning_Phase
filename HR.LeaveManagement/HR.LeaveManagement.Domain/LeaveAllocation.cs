75b4c965002ca871850699545fa9da2eb0da8aa3
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-04-17 23:20:02 +0000

    Created model classes

---
+++ HR.LeaveManagement/HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain
+{
+   public class LeaveAllocation
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ HR.LeaveManagement/HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id {  get; set; }
+
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
